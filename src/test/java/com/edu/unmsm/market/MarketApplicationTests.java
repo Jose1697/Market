af4d0a020684dbf7c3e0f01eebdcf61aed928d10
Jose1697 <josenetboy@hotmail.com>
2021-11-17 15:28:19 -0500

    9. Hello World in Spring HolaMundoController

---
+++ src/test/java/com/edu/unmsm/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.unmsm.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
