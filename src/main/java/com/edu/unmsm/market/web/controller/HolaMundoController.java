22c13133750e85395040de9caaaa5f584dee6f7a
Jose1697 <josenetboy@hotmail.com>
2021-11-17 16:05:06 -0500

    11. creacion de la estructura del proyecto

--- src/main/java/com/edu/unmsm/market/web/controller/HolaMundoController.java
+++ src/main/java/com/edu/unmsm/market/web/controller/HolaMundoController.java
@@ -1,4 +1,4 @@
-package com.edu.unmsm.market;
+package com.edu.unmsm.market.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
